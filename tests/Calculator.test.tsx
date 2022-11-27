62e08131ee6ead7a9781f41918f9f51008640cab
shoxie <sktt1lka@gmail.com>
2022-11-27 14:21:40 +0700

    fix test

--- tests/Calculator.test.tsx
+++ tests/Calculator.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, act } from "@testing-library/react";
 import Calculator from "../src/modules/Calculator";
 
 describe("Calculator", () => {
@@ -13,19 +13,19 @@ describe("Calculator", () => {
     expect(screen.getByTestId("multiply")).toBeInTheDocument();
     expect(screen.getByTestId("divide")).toBeInTheDocument();
   });
-  it("can add numbers", async () => {
+  it("can add numbers", () => {
     render(<Calculator />);
     const inputa = screen.getByTestId("number-a");
     const inputb = screen.getByTestId("number-b");
     const button = screen.getByTestId("add");
 
-    fireEvent.change(inputa, { target: { value: 10 } });
-    fireEvent.change(inputb, { target: { value: 20 } });
-    button.click();
-    await waitFor(() => {
-      const result = screen.getByTestId("result");
-      expect(result).toHaveTextContent("30");
+    act(() => {
+      fireEvent.change(inputa, { target: { value: 10 } });
+      fireEvent.change(inputb, { target: { value: 20 } });
+      button.click();
     });
+    const result = screen.getByTestId("result");
+    expect(result).toHaveTextContent("30");
   });
 
   it("can subtract numbers", async () => {
@@ -34,13 +34,14 @@ describe("Calculator", () => {
     const inputb = screen.getByTestId("number-b");
     const button = screen.getByTestId("subtract");
 
-    fireEvent.change(inputa, { target: { value: 20 } });
-    fireEvent.change(inputb, { target: { value: 10 } });
-    button.click();
-    await waitFor(() => {
-      const result = screen.getByTestId("result");
-      expect(result).toHaveTextContent("10");
+    act(() => {
+      fireEvent.change(inputa, { target: { value: 20 } });
+      fireEvent.change(inputb, { target: { value: 10 } });
+      button.click();
     });
+
+    const result = screen.getByTestId("result");
+    expect(result).toHaveTextContent("10");
   });
 
   it("can multiply numbers", async () => {
@@ -49,13 +50,14 @@ describe("Calculator", () => {
     const inputb = screen.getByTestId("number-b");
     const button = screen.getByTestId("multiply");
 
-    fireEvent.change(inputa, { target: { value: 10 } });
-    fireEvent.change(inputb, { target: { value: 20 } });
-    button.click();
-    await waitFor(() => {
-      const result = screen.getByTestId("result");
-      expect(result).toHaveTextContent("200");
+    act(() => {
+      fireEvent.change(inputa, { target: { value: 10 } });
+      fireEvent.change(inputb, { target: { value: 20 } });
+      button.click();
     });
+
+    const result = screen.getByTestId("result");
+    expect(result).toHaveTextContent("200");
   });
 
   it("can divide numbers", async () => {
@@ -64,12 +66,12 @@ describe("Calculator", () => {
     const inputb = screen.getByTestId("number-b");
     const button = screen.getByTestId("divide");
 
-    fireEvent.change(inputa, { target: { value: 20 } });
-    fireEvent.change(inputb, { target: { value: 10 } });
-    button.click();
-    await waitFor(() => {
-      const result = screen.getByTestId("result");
-      expect(result).toHaveTextContent("2");
+    act(() => {
+      fireEvent.change(inputa, { target: { value: 20 } });
+      fireEvent.change(inputb, { target: { value: 10 } });
+      button.click();
     });
+    const result = screen.getByTestId("result");
+    expect(result).toHaveTextContent("2");
   });
 });
